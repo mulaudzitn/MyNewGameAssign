b75c220742375d0a407a0b3b7aa867557aba23b8
Norman Mulaudzi <mulaudzitn@gmail.com>
2016-04-22 16:20:00 +0200

    Add the UpdateThread Class

--- RainDodgeGame/src/com/wits/edu/Elen7045/GamePlayerControls/GameKeyListener.java
+++ RainDodgeGame/src/com/wits/edu/Elen7045/GamePlayerControls/GameKeyListener.java
@@ -1,9 +1,5 @@
 package com.wits.edu.Elen7045.GamePlayerControls;
 
-import com.wits.edu.Elen7045.GameLevels.GameLevelFactory;
-import com.wits.edu.Elen7045.GameLevels.GameLevelInterface;
-import com.wits.edu.Elen7045.gameScreen.GamePanel;
-
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
@@ -11,9 +7,9 @@ import java.awt.event.KeyListener;
  * Created by F3694941 on 2016/04/20.
  */
 public class GameKeyListener implements KeyListener {
-    private int current_X_Axis;
-    private GameLevelFactory gameLevelFactory = new GameLevelFactory();
-    private GameLevelInterface gameLevel = gameLevelFactory.getGameLevel("Level1");
+    private int current_X_Axis = 350;
+    //   private GameLevelFactory gameLevelFactory = new GameLevelFactory();
+    //   private GameLevelInterface gameLevel = gameLevelFactory.getGameLevel("Level1");
 
     @Override
     public void keyTyped(KeyEvent e) {
@@ -23,16 +19,18 @@ public class GameKeyListener implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
           int key = e.getKeyCode();
+        //Moving player to the left
         if(key == KeyEvent.VK_LEFT)
         {
 
             current_X_Axis -=15 ;
-            System.out.println("Moving Left");
+
         }
+        // moving player to the right
         if(key == KeyEvent.VK_RIGHT)
         {
             current_X_Axis +=15;
-            System.out.println("Moving Right");
+
         }
 
     }
@@ -41,14 +39,14 @@ public class GameKeyListener implements KeyListener {
     public void keyReleased(KeyEvent e) {
 
     }
-    public void setCurrent_X_Axis( int xAxis)
-    {
-        current_X_Axis = xAxis;
-    }
 
     public int getCurrent_X_Axis()
     {
         return  current_X_Axis;
+    }
+
+    public void setCurrent_X_Axis(int xAxis) {
+        current_X_Axis = xAxis;
     }
 
 }
