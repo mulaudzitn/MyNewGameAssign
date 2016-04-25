cd2e6ead6b8b32e06b4f099aa67ea1675794222e
Norman Mulaudzi <mulaudzitn@gmail.com>
2016-04-25 15:12:41 +0200

    Change the Ant build settings

--- RainDodgeGame/src/com/wits/edu/Elen7045/GameLevels/GameLevelFactory.java
+++ RainDodgeGame/src/com/wits/edu/Elen7045/GameLevels/GameLevelFactory.java
@@ -2,9 +2,6 @@ package com.wits.edu.Elen7045.GameLevels;
 
 
 
-/**
- * Created by Norman Mulaudzi on 2016/04/18.
- */
 public class GameLevelFactory {
     public GameLevelInterface getGameLevel(String level)
     {
